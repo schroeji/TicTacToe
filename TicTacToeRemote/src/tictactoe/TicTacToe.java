78944f73c8ae826e18db4df7cdd911f9f619dcd2
schroeji <schroeji@studi.informatik.uni-stuttgart.de>
2018-04-17 14:24:21 +0100

    Removed author comments

--- TicTacToeRemote/src/tictactoe/TicTacToe.java
+++ TicTacToeRemote/src/tictactoe/TicTacToe.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tictactoe;
 
 import java.awt.GridLayout;
@@ -12,10 +7,6 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author hidden
- */
 public class TicTacToe extends javax.swing.JFrame {
 
     /**
